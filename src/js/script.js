470ab0f56e505db0d48702d3361204a4383de915
uncorre <yuriy.ponomarev224@gmail.com>
2021-11-14 22:42:16 +0600

    me
    done modal and js

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,12 @@ function myFunction() {
     document.getElementById("box_btn").classList.toggle("box__rotatebtn")
     document.getElementById("box_content").classList.toggle("box__show");
 }
+function modalWidow() {
+    document.getElementById("modalbox").classList.toggle("modalbox__show")
+}
+var modal = document.getElementById("modalbox");
+window.onclick = function(event) {
+    if (event.target == modal) {
+        document.getElementById("modalbox").classList.toggle("modalbox__show");
+    }
+}
