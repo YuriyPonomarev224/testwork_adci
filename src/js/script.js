4466e0af14f898f4f40f17ab25340b8381fec09a
uncorre <yuriy.ponomarev224@gmail.com>
2021-12-18 15:15:22 +0600

    Fix
    Rename function : now functions reflect their actions
    
    Moved the event handlers to a separate file
    
    Fixed the value of the name attribute

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
-function myFunction() {
+window.onload = init;
+
+function showBox() {
     document.getElementById("box_btn").classList.toggle("box__rotatebtn")
     document.getElementById("box_content").classList.toggle("box__show");
 }
@@ -11,3 +13,13 @@ window.onclick = function(event) {
         document.getElementById("modalbox").classList.toggle("modalbox__show");
     }
 }
+
+function init() {
+    var BoxBtn = document.getElementById("box_btn");
+    var ModalBtn = document.getElementById("Modal_Exit");
+    var HeaderModalBtn = document.getElementById("Header_Modal_Btn");
+    BoxBtn.onclick = showBox;
+    ModalBtn.onclick = modalWidow;
+    HeaderModalBtn.onclick = modalWidow;
+}
+
